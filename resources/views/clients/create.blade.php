f16b745684f101ff4fea3760694cf34d25d13442
nardsqq <nardparagas@gmail.com>
2019-03-27 15:20:06 +0800

    Change default form input id from inputAddress into address

--- resources/views/clients/create.blade.php
+++ resources/views/clients/create.blade.php
@@ -7,7 +7,7 @@
     <a href="{{ route('clients.index') }}" class="float-right text-dark"><< Return to the Clients page</a>
 @endsection
 
-<section class="col-md-12 my-3">
+<section class="col-md-9 my-3">
     <div class="card">
         <div class="card-body">
             <form>
@@ -22,8 +22,8 @@
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="inputAddress">Address</label>
-                    <input type="text" class="form-control" id="inputAddress" placeholder="#01 Main St, Brgy. 101, Metro City, Central Region">
+                    <label for="address">Address</label>
+                    <input type="text" class="form-control" id="address" placeholder="#01 Main St, Brgy. 101, Metro City, Central Region">
                 </div>
                 <div class="form-row">
                     <div class="form-group col-md-6">
