ff4a1afc950bf864ee0b85168a2c2325a0bb0e17
nardsqq <nardparagas@gmail.com>
2019-03-27 18:05:14 +0800

    Fix login and registration page layouts

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('/clients', 'ClientsController');
+Route::resource('/clients', 'ClientsController')->middleware('auth');
