ce8fb7a0dbe55567313f26eeb79b76e64d757d36
nardsqq <nardparagas@gmail.com>
2019-03-29 02:04:29 +0800

    Conditionally render dashboard for performers

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace Appoint\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Appoint\User;
+
 class HomeController extends Controller
 {
     /**
@@ -23,6 +25,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $performers = User::where('role', 1)->get();
+
+        return view('home', compact('performers'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,28 +6,48 @@
     Welcome to <strong>Appoint</strong>, a lightweight events booking system. <a href="#" class="text-secondary"><strong>Create</strong></a> an event now.
 @endsection
 
-<section class="col-md-4">
-    <div class="shadow-sm p-3 mb-5 bg-white rounded">
-        <h3>Upcoming Events</h3>
-        <p class="h1">
-            0
-        </p>
-    </div>
-</section>
-<section class="col-md-4">
-    <div class="shadow-sm p-3 mb-5 bg-white rounded">
-        <h3>Available Performers</h3>
-        <p class="h1">
-            0
-        </p>
-    </div>
-</section>
-<section class="col-md-4">
-    <div class="shadow-sm p-3 mb-5 bg-white rounded">
-        <h3>Active Bookings</h3>
-        <p class="h1">
-            0
-        </p>
-    </div>
-</section>
+@if (Auth::user()->role == 0)
+    <section class="col-md-4">
+        <div class="shadow-sm p-3 mb-5 bg-white rounded">
+            <h3>Upcoming Events</h3>
+            <p class="h1">
+                0
+            </p>
+        </div>
+    </section>
+    <section class="col-md-4">
+        <div class="shadow-sm p-3 mb-5 bg-white rounded">
+            <h3>Available Performers</h3>
+            <p class="h1">
+                {{ $performers->count() }}
+            </p>
+        </div>
+    </section>
+    <section class="col-md-4">
+        <div class="shadow-sm p-3 mb-5 bg-white rounded">
+            <h3>Active Bookings</h3>
+            <p class="h1">
+                0
+            </p>
+        </div>
+    </section>
+@elseif (Auth::user()->role == 1)
+    <section class="col-md-6">
+        <div class="shadow-sm p-3 mb-5 bg-white rounded">
+            <h3>Upcoming Events</h3>
+            <p class="h1">
+                0
+            </p>
+        </div>
+    </section>
+    <section class="col-md-6">
+        <div class="shadow-sm p-3 mb-5 bg-white rounded">
+            <h3>Active Bookings</h3>
+            <p class="h1">
+                0
+            </p>
+        </div>
+    </section>
+@endif
+
 @endsection
