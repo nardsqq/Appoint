2514f6975917b10cd1c37961a2bf48eff3622e3d
nardsqq <nardparagas@gmail.com>
2019-03-29 02:17:33 +0800

    Update eloquent relationships of Booking model

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -3,8 +3,25 @@
 namespace Appoint\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+use Appoint\User;
+use Appoint\Models\Event;
 
 class Booking extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $guarded = [];
+    protected $dates = ['deleted_at'];
+    
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function event()
+    {
+        return $this->belongsTo(Event::class);
+    }
 }
